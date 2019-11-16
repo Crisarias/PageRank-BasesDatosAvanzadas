837a74480cfa8f87ae92ed2d47872b47bbe6bc65
YoelRP <josejohelrodriguez@gmail.com>
2019-11-16 10:31:49 -0600

    Generador last V

--- generador2.py
+++ generador2.py
@@ -8,22 +8,15 @@ CantNodos = int(input("Cantidad de nodos "))
 file = open("./filename.txt", "w")
 #CantNodos = random.randrange(100)
 print(CantNodos)
-<<<<<<< Updated upstream
-=======
 file.write(str(CantNodos))
 file.write(os.linesep)
->>>>>>> Stashed changes
 #file.write(str(CantNodos)+os.linesep)
 for lineas in range(CantNodos):
     cantvertex =  random.randrange(CantNodos)
     
  #   print(lineas,end = '')
     print(" ",end = '')
-<<<<<<< Updated upstream
-    file.write(str(lineas) + " ")
-=======
     #file.write(str(lineas) + " ")
->>>>>>> Stashed changes
     lista = []
     for x in range(cantvertex):
         linkto= random.randrange(CantNodos)
